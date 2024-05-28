c1d0e9887b2b05bfb25a0a27fc3508b8a7b87b87
Tim van der Leeuw <tim@wearefrank.nl>
2024-05-28 19:35:02 +0200

    Use randomly generated IDs in all tests to make sure they're independent.

--- core/src/test/java/org/frankframework/jdbc/TransactionManagerTest.java
+++ core/src/test/java/org/frankframework/jdbc/TransactionManagerTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.sql.Connection;
 
+import org.apache.commons.lang3.RandomUtils;
 import org.frankframework.testutil.JdbcTestUtil;
 import org.frankframework.testutil.junit.DatabaseTestEnvironment;
 import org.frankframework.testutil.junit.TxManagerTest;
@@ -14,14 +15,14 @@ import org.springframework.transaction.TransactionStatus;
 public class TransactionManagerTest {
 	private static final String TEST_TABLE = "tralala";
 
-	protected void checkNumberOfLines(DatabaseTestEnvironment env, int expected) throws Exception {
-		checkNumberOfLines(env, expected, "select count(*) from " + TEST_TABLE + " where TKEY = 1");
+	protected void checkNumberOfLines(DatabaseTestEnvironment env, int expected, int key) throws Exception {
+		checkNumberOfLines(env, expected, "select count(*) from " + TEST_TABLE + " where TKEY = ?", key);
 	}
 
-	private void checkNumberOfLines(DatabaseTestEnvironment env, int expected, String query) throws Exception {
+	private void checkNumberOfLines(DatabaseTestEnvironment env, int expected, String query, Object... params) throws Exception {
 		String preparedQuery = env.getDbmsSupport().prepareQueryTextForNonLockingRead(query);
 		try(Connection connection = env.getConnection()) {
-			int count = JdbcTestUtil.executeIntQuery(connection, preparedQuery);
+			int count = JdbcTestUtil.executeIntQuery(connection, preparedQuery, params);
 			assertEquals(expected, count, "number of lines in table");
 		}
 	}
@@ -29,77 +30,85 @@ public class TransactionManagerTest {
 	@TxManagerTest
 	@WithLiquibase(file = "Migrator/ChangelogBlobTests.xml", tableName = TEST_TABLE)
 	public void testCommit(DatabaseTestEnvironment env) throws Exception {
+		final int key = RandomUtils.nextInt();
 		try(Connection connection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=1");
+			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=?", key);
 		}
 
 		TransactionStatus txStatus = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRED);
 
 		try (Connection txManagedConnection = env.getConnection()) {
-			checkNumberOfLines(env, 0);
-			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (1)");
+			checkNumberOfLines(env, 0, key);
+			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (?)", key);
 		}
 
 		env.getTxManager().commit(txStatus);
 
-		checkNumberOfLines(env, 1);
+		checkNumberOfLines(env, 1, key);
 	}
 
 	@TxManagerTest
 	@WithLiquibase(file = "Migrator/ChangelogBlobTests.xml", tableName = TEST_TABLE)
 	public void testRollback(DatabaseTestEnvironment env) throws Exception {
+		final int key = RandomUtils.nextInt();
+		TransactionStatus txPrepareStatus = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRED);
 		try(Connection connection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=1");
+			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=?", key);
 		}
+		checkNumberOfLines(env, 0, key);
+		env.getTxManager().commit(txPrepareStatus);
 
 		TransactionStatus txStatus = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRED);
 
 		try (Connection txManagedConnection = env.getConnection()) {
-			checkNumberOfLines(env, 0);
-			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (1)");
-//			checkNumberOfLines(env, 0);
+			checkNumberOfLines(env, 0, key);
+			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (?)", key);
+			checkNumberOfLines(env, 1, key);
 		}
-//		checkNumberOfLines(env, 0);
+		checkNumberOfLines(env, 1, key);
 
 		env.getTxManager().rollback(txStatus);
 
-		checkNumberOfLines(env, 0);
+		checkNumberOfLines(env, 0, key);
 	}
 
 	@TxManagerTest
 	@WithLiquibase(file = "Migrator/ChangelogBlobTests.xml", tableName = TEST_TABLE)
 	public void testRequiresNew(DatabaseTestEnvironment env) throws Exception {
+		final int key1 = RandomUtils.nextInt();
+
 		try(Connection connection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=1");
+			JdbcTestUtil.executeStatement(connection, "DELETE FROM " + TEST_TABLE + " where TKEY=?", key1);
 		}
 
 		try (Connection txManagedConnection = env.getConnection()) {
-			checkNumberOfLines(env, 0);
-			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (1)");
+			checkNumberOfLines(env, 0, key1);
+			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (?)", key1);
 		}
 
 		TransactionStatus txStatus1 = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRED);
 
 		try (Connection txManagedConnection = env.getConnection()) {
-			checkNumberOfLines(env, 1);
-			JdbcTestUtil.executeStatement(txManagedConnection, "UPDATE " + TEST_TABLE + " SET TVARCHAR='tralala' WHERE tkey=1");
+			checkNumberOfLines(env, 1, key1);
+			JdbcTestUtil.executeStatement(txManagedConnection, "UPDATE " + TEST_TABLE + " SET TVARCHAR='tralala' WHERE tkey=?", key1);
 		}
 
 		try (Connection txManagedConnection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(txManagedConnection, "SELECT TVARCHAR FROM " + TEST_TABLE + " WHERE tkey=1");
+			JdbcTestUtil.executeStatement(txManagedConnection, "SELECT TVARCHAR FROM " + TEST_TABLE + " WHERE tkey=?", key1);
 		}
-		checkNumberOfLines(env, 1);
+		checkNumberOfLines(env, 1, key1);
 
+		final int key2 = RandomUtils.nextInt();
 		TransactionStatus txStatus2 = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
 		try (Connection txManagedConnection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (2)");
+			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (?)", key2);
 		}
 
 		env.getTxManager().commit(txStatus2);
 		env.getTxManager().commit(txStatus1);
 
-		checkNumberOfLines(env, 1);
-		checkNumberOfLines(env, 1, "select count(*) from " + TEST_TABLE + " where TKEY = 2");
+		checkNumberOfLines(env, 1, key1);
+		checkNumberOfLines(env, 1, key2);
 	}
 
 	@TxManagerTest
@@ -109,14 +118,16 @@ public class TransactionManagerTest {
 		// This tests fails for Narayana, if no Modifiers are present for the database driver.
 		// @see NarayanaDataSourceFactory.checkModifiers()
 
+		final int key1 = RandomUtils.nextInt();
 		TransactionStatus txStatusOuter = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRED);
 		try (Connection txManagedConnection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(txManagedConnection, "SELECT TVARCHAR FROM " + TEST_TABLE + " WHERE tkey=1");
+			JdbcTestUtil.executeStatement(txManagedConnection, "SELECT TVARCHAR FROM " + TEST_TABLE + " WHERE tkey=?", key1);
 		}
 
+		final int key2 = RandomUtils.nextInt();
 		TransactionStatus txStatusInner = env.startTransaction(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
 		try (Connection txManagedConnection = env.getConnection()) {
-			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (2)");
+			JdbcTestUtil.executeStatement(txManagedConnection, "INSERT INTO " + TEST_TABLE + " (tkey) VALUES (?)", key2);
 		}
 
 		env.getTxManager().commit(txStatusInner);
